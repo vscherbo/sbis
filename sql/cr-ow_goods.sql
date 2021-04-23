325e31f9172f51f8967542cc4d226f46b574a35e
Vladimir Scherbo <vscherbo@gmail.com>
2021-04-23 13:36:23 +0300

    owen_id is PK

--- sql/cr-ow_goods.sql
+++ sql/cr-ow_goods.sql
@@ -3,15 +3,12 @@
 -- DROP TABLE ext.ow_goods;
 
 CREATE TABLE ext.ow_goods (
-    id serial NOT NULL,
-    owen_id int4 NULL,
+    owen_id int4 NOT NULL,
     name_short varchar NULL,
     name_full varchar NULL,
     "КодСодержания" int4 NULL,
     who int4 NULL,
     dt_sync timestamp(0) NULL,
-    mod_id varchar NULL
+    CONSTRAINT ow_goods_pk PRIMARY KEY (owen_id)
 );
-COMMENT ON TABLE ext.ow_goods IS 'no_dump';
-
 -- CREATE INDEX trgm_idx ON ext.ow_goods USING gist (name_short gist_trgm_ops);
